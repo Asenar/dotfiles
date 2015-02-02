aa63d18eacafe8f8ccc6a223614aea19c74dc7a4
Michaël Marinetti <github@marinetti.fr>
2015-02-02 18:56:47 +0100

    s/LG/keyboard i3

--- i3/keymap.sh
+++ i3/keymap.sh
@@ -6,5 +6,5 @@ i3status --config ~/.i3/i3status-light | while :
 do
         read line
         LG=$(setxkbmap -print | grep xkb_symbols | awk -F"+" '{print $2}') 
-        echo "LG: $LG | $line" || exit 1
+        echo "keyboard: $LG | $line" || exit 1
 done
